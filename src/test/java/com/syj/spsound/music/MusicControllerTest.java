fdff33b102ef124c3944f301eae2f736b14e5c7e
yj <yj>
2024-04-23 18:47:09 +0900

    [송여진] - 스포티파이 api 토큰 발급 구현

---
+++ src/test/java/com/syj/spsound/music/MusicControllerTest.java
@@ -0,0 +1,24 @@
+package com.syj.spsound.music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicControllerTest {
+
+	// 테스트 케이스 설계
+	// 메소드 수행 - 리턴값이 null 이 아니다.
+	
+	@Test
+	public void musicTest() {
+
+//		private Logger logger = LoggerFactory.getLogger(getClass());
+//		logger.info();
+		
+//		Like like = likeService.addLike(2, 4);
+//		assertNotNull(like);
+//		assertEquals(count, 3);
+		
+	}
+	
+}
