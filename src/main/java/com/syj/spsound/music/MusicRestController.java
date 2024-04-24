867c12442e7f048f8958c69bcd3a3443292cc528
yj <yj>
2024-04-24 18:47:36 +0900

    [송여진] - 스포티파이 api 설정중

--- src/main/java/com/syj/spsound/music/MusicRestController.java
+++ src/main/java/com/syj/spsound/music/MusicRestController.java
@@ -4,20 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import com.syj.spsound.common.CreateToken;
 import com.syj.spsound.music.service.MusicService;
 
 import jakarta.servlet.http.HttpSession;
@@ -108,25 +100,5 @@ public class MusicRestController {
 		
 		return resultMap;		
 	}
-	
-	@GetMapping("/search")
-	public String Search() {
 		
-		String accessToken = CreateToken.accesstoken();
-		
-        RestTemplate rest = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Bearer " + accessToken);
-        headers.add("Host", "api.spotify.com");
-        headers.add("Content-type", "application/json");
-        String body = "";
-
-        HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
-        ResponseEntity<String> responseEntity = rest.exchange("https://api.spotify.com/v1/search?type=track&q=" + "peaches", HttpMethod.GET, requestEntity, String.class);
-        HttpStatus httpStatus = (HttpStatus) responseEntity.getStatusCode();
-        int status = httpStatus.value(); //상태 코드가 들어갈 status 변수
-        String response = responseEntity.getBody();
-
-        return response;
-	}	
 }
--- src/main/java/com/syj/spsound/spotify/service/SpotifyService.java
+++ src/main/java/com/syj/spsound/spotify/service/SpotifyService.java
@@ -1,19 +1,22 @@
-package com.syj.spsound.common;
+package com.syj.spsound.spotify.service;
 
 import java.io.IOException;
 
 import org.apache.hc.core5.http.ParseException;
+import org.springframework.stereotype.Service;
 
 import com.wrapper.spotify.SpotifyApi;
 import com.wrapper.spotify.exceptions.SpotifyWebApiException;
 import com.wrapper.spotify.model_objects.credentials.ClientCredentials;
 import com.wrapper.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
 
-public class CreateToken {
-	
+@Service
+public class SpotifyService {
+
     private static final String CLIENT_ID = "153f64de7d1d4018b5245050eaf8bf9a";
     private static final String CLIENT_SECRET = "38ceb3923c414ca6ae5a00ea6fc15fb3";
     private static final SpotifyApi spotifyApi = new SpotifyApi.Builder().setClientId(CLIENT_ID).setClientSecret(CLIENT_SECRET).build();
+
 
     public static String accesstoken() {
         ClientCredentialsRequest clientCredentialsRequest = spotifyApi.clientCredentials().build();
@@ -29,4 +32,12 @@ public class CreateToken {
         }
     }
 	
+	public void GetSpotifyApi() {
+		
+//		String accessToken = SpotifyToken.accesstoken();
+				
+		spotifyApi.setAccessToken(accesstoken());
+
+		spotifyApi.getAccessToken();
+	}	
 }
