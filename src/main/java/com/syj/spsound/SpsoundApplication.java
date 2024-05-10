a1e14dc93c7560dc4f0133d3770bcbc68f33f7a1
DESKTOP-G99OQ4S\user <syj@naver.com>
2024-05-10 17:07:15 +0900

    [송여진] - 설정 변경

--- src/main/java/com/syj/spsound/SpsoundApplication.java
+++ src/main/java/com/syj/spsound/SpsoundApplication.java
@@ -2,9 +2,10 @@ package com.syj.spsound;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpsoundApplication {
+public class SpsoundApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpsoundApplication.class, args);
